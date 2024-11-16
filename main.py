17c072ca0191829893e7f37f7a96b57dc1e4752b
Daniil4090 <russiatop8902@gmail.com>
2024-11-16 19:50:54 +0800

    Добавление проверок ключеей

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from flask import Flask, request
 import sqlite3
 import os
+import random
+
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LETTERS += LETTERS.lower()
+LETTERS +="1234567890"
 
 app = Flask(__name__)
 
+
+def generate_key(a):
+    key = ""
+    for i in range(a):
+        key += random.choice(LETTERS)
+    return key
+
+
 @app.route('/')
 def index():
+    auth_key = request.args.get("auth_key")
+    if auth_key != key:
+        print(" * Запрос отклонен.")
+        return "Запрос отклонен."
     return 'Приветствуем в нашей панели управления!'
 
 
 @app.route('/add_device', methods=['post', 'get'])
 def add_device():
+    auth_key = request.args.get("auth_key")
+    if auth_key != key:
+        print(" * Запрос отклонен.")
+        return "Запрос отклонен."
     device_name = request.args.get("device_name")
     device_address = request.args.get("device_address")
     device_apiKey = request.args.get("device_apiKey")
@@ -30,6 +51,10 @@ def add_device():
 
 @app.route('/delete_device')
 def delete_device():
+    auth_key = request.args.get("auth_key")
+    if auth_key != key:
+        print(" * Запрос отклонен.")
+        return "Запрос отклонен."
     device_id = request.args.get("device_id")
     print(f" ! Запрос на удаление устройства под ID: {device_id}.")
     try:
@@ -42,6 +67,21 @@ def delete_device():
 
 
 if __name__ == "__main__":
+    try:
+        print(" * Чтение ключа...")
+        config = open("config.txt", "r")
+        key = config.read()
+        config.close()
+        print(" * Выполнено!")
+    except FileNotFoundError:
+        print(" * Возникла ошибка при чтении. Идет создание конфигурации...")
+        key = generate_key(16)
+        config = open("config.txt", "w+")
+        config.write(key)
+        config.close()
+        print(" * Выполнено!")
+    print(" * Ключ для взаимодействия с панелью:", key)
+    print(" ! НИКОМУ НЕ СООБЩАЙТЕ ЭТОТ КЛЮЧ!")
     print(" * Подключение базы данных...")
     con = sqlite3.connect("Devices/HomePanel.db", check_same_thread=False)
     cur = con.cursor()
